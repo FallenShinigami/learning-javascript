685506db40e9ea39f397bd83484a9843c2934f6c
Yesharth Bisht <yesharthb@gmail.com>
2026-09-19 12:59:01 +0530

    learnt switch

--- script.js
+++ script.js
@@ -166,3 +166,28 @@
 // } else {
 //   alert("i dont know you");
 // }
+
+// let browser= prompt("what is your browser",'Edge')
+// if (browser=='Edge'){
+//     alert("You've got the Edge!");
+// }
+// else if(browser = 'chrome'|| browser == 'firefox' ||browser== 'safari'||browser == 'opera'){
+//     alert("Okay we support this browsers too");
+// }
+// else {
+//     alert('We hope that this page looks ok!' );
+// }
+
+let a = +prompt("a?", "");
+switch (a) {
+  case 0:
+    alert(0);
+    break;
+  case 1:
+    alert(1);
+    break;
+  case 2:
+  case 3:
+    alert("3");
+    break;
+}
