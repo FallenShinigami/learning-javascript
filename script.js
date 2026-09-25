c969e3be2673bf2ba3f31282c404e61887c686f6
Yesharth Bisht <yesharthb@gmail.com>
2026-09-25 17:15:11 +0530

    object learning

--- script.js
+++ script.js
@@ -178,16 +178,23 @@
 //     alert('We hope that this page looks ok!' );
 // }
 
-let a = +prompt("a?", "");
-switch (a) {
-  case 0:
-    alert(0);
-    break;
-  case 1:
-    alert(1);
-    break;
-  case 2:
-  case 3:
-    alert("3");
-    break;
-}
+// let a = +prompt("a?", "");
+// switch (a) {
+//   case 0:
+//     alert(0);
+//     break;
+//   case 1:
+//     alert(1);
+//     break;
+//   case 2:
+//   case 3:
+//     alert("3");
+//     break;
+// }
+let user = {
+  name: "John",
+  surname: "Smith",
+};
+
+user.name = "Pete";
+delete user.name;
